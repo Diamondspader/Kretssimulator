6def087204fd2b71977c1221924342657c707663
xiali651 <xiali651@tlhw-4-2.ad.liu.se>
2020-03-02 13:54:06 +0100

    commiy

--- Simulator.cc
+++ Simulator.cc
@@ -1,21 +1,18 @@
 #include "Component.h"
 #include <iostream>
-
+#include <sstream>
 
 
 int main(int argc, char** argv)
 {
-  std::cout << "You have entered " << argc
-     << " arguments:" << "\n";
-
-     for (int i = 0; i < argc; ++i)
-     {
-       if(argv[i] < 0)
-       {
+  for (int i = 0; i < argc; ++i)
+  {
+    if(argv[i] < 0)
+    {
          //throw exception
-       }
-       std::cout << argv[i] << "\n";
-       //first iterations, outputs, steps, Volt
-     }
+    }
+  }
+  simluate();
+
 return 0;
 }
